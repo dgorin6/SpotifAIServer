1c2295a8e4addde78929c3b5368c8f20d884cc76
David <dgorin6@gatech.edu>
2023-09-01 16:01:03 -0400

    prompt engineering

--- src/index.js
+++ src/index.js
@@ -27,30 +27,32 @@ app.get('/', async (req, res) => {
 app.post('/playlist', async (req, res) => {
     const description = req.body.description;
     const model_prompt = `
-    Generate a playlist based on the given description and format it as a CSV with the song name followed by the artist. If the input doesn't make sense, return False. + 
+    You are a playlist generating robot. In order to generate a playlist, you will take in a description as an input. You will have 2 states, awaiting and generating. In your awaiting state, ask for a description. Once a description is provided, you can have 2 possible outputs. The first is a generated playlist. Your playlist output must look exactly as below. NO ADDITONAL OUTPUT TEXT. Do not write anything before the playlist or after. However, if the input prompt does not make any sense in a playlist context, just output FALSE.
 
-    Description: "${description}"
-    
-    Playlist Format:
-    [song_1_name], [artist_1]
-    [song_2_name], [artist_2]
-    [song_3_name], [artist_3]
-    ...
+    Output Format:  song_1_name,artist_1,song_2_name,artist_2 ,song_3_name,artist_3,...
+
+    Sample Input: 80's greatest hits
+
+    Sample Output: Don't Stop Believin',Journey, Billie Jean,Michael Jackson,Eye of the Tiger, Survivor, Sweet Child o' Mine,Guns N' Roses,Every Breath You Take,The Police, Livin' on a Prayer,Bon Jovi
+
+    Sample Input 2: safndanda
+
+    Sample Output: FALSE
 `;
 console.log(model_prompt)
   try {
     if (description == null) {
-      throw new Error("Uh oh, no prompt was provided");
+      throw new Error("Uh oh, no prom`pt was provided");
     }
     const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [{
           "role": "system",
-          "content": "You are a helpful assistant."
+          "content": model_prompt
         },
         {
           "role": "user",
-          "content": model_prompt
+          "content": description
         }],
       });
       console.log(completion)

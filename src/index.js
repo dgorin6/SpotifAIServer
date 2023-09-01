fdf5d5f6762d01d4d763974195a27f25704aa7a7
David <dgorin6@gatech.edu>
2023-09-01 19:24:42 -0400

    prompt + output standardization

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
   });
 
+function standardizeOutput(input) {
+    // Remove double quotes, backslashes, and extra spaces around commas
+    const standardized = input.replace(/"/g, '').replace(/\\/g, '').replace(/\s*,\s*/g, ',').replace(/\n/g, '');
+  
+    return standardized;
+  }
 async function authenticateWithSpotify() {
   const authResponse = await axios.post('https://accounts.spotify.com/api/token', null, {
     params: {
@@ -21,42 +27,48 @@ async function authenticateWithSpotify() {
 
   return authResponse.data.access_token;
 }
+
+async function getPlaylist(description) {
+  const system_prompt = `
+    You are a playlist generating robot. In order to generate a playlist, you will take in a description as an input.
+    This description could be a theme, a pattern, etc. To the best of your ability, create a playlist using this description.
+  `;
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+    {"role": "system", "content": "you are a helpful assistant"},
+    {"role": "user","content": system_prompt},
+    {"role": "user","content": "Awaiting description"},
+    {"role": "user","content": description},
+  ],
+  });
+  const unformatted_playlist = completion.choices[0].message.content
+  console.log(unformatted_playlist)
+  const format_prompt = `Your role is to convert this text into a format that I can use for data processing. You will recieve output from another LLM which contains a music playlist. Return the playlist to me in the below format.
+  The only text you output should be text that was input for data processing. Do not acknowledge my request or add additional text. \n
+  format: song_1_name,artist_1,song_2_name,artist_2 ,song_3_name,artist_3,...\n
+  example: Don't Stop Believin',Journey,Billie Jean,Michael Jackson,Eye of the Tiger,Survivor, Sweet Child o' Mine,Guns N' Roses,Every Breath You Take,The Police, Livin' on a Prayer,Bon Jovi`;
+
+  const format_completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+    {"role": "system", "content": format_prompt},
+    {"role": "user","content": unformatted_playlist},
+  ],
+  });
+  return standardizeOutput(format_completion.choices[0].message.content);
+}
 app.get('/', async (req, res) => {
   res.json({ message: 'Hello from SpotifAI!' });
 });
 app.post('/playlist', async (req, res) => {
-    const description = req.body.description;
-    const model_prompt = `
-    You are a playlist generating robot. In order to generate a playlist, you will take in a description as an input. You will have 2 states, awaiting and generating. In your awaiting state, ask for a description. Once a description is provided, you can have 2 possible outputs. The first is a generated playlist. Your playlist output must look exactly as below. NO ADDITONAL OUTPUT TEXT. Do not write anything before the playlist or after. However, if the input prompt does not make any sense in a playlist context, just output FALSE.
-
-    Output Format:  song_1_name,artist_1,song_2_name,artist_2 ,song_3_name,artist_3,...
-
-    Sample Input: 80's greatest hits
-
-    Sample Output: Don't Stop Believin',Journey, Billie Jean,Michael Jackson,Eye of the Tiger, Survivor, Sweet Child o' Mine,Guns N' Roses,Every Breath You Take,The Police, Livin' on a Prayer,Bon Jovi
-
-    Sample Input 2: safndanda
-
-    Sample Output: FALSE
-`;
-console.log(model_prompt)
+  const description = req.body.description;
   try {
     if (description == null) {
       throw new Error("Uh oh, no prom`pt was provided");
     }
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{
-          "role": "system",
-          "content": model_prompt
-        },
-        {
-          "role": "user",
-          "content": description
-        }],
-      });
-      console.log(completion)
-      res.json({"playlist": completion.choices[0].message.content});
+    const playlist = await getPlaylist(description)
+    return res.json({playlist: playlist})
   } catch (error) {
     console.log(error.message);
   }
